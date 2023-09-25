d5e69b61d45f0c8505bbc494b2f3b68084d2018f
Hyperbolic12 <141738577+Hyperbolic12@users.noreply.github.com>
2023-09-25 12:08:42 +0530

    first commit

---
+++ webpack---/src/App-6c11247539598.vue
@@ -0,0 +1,8 @@
+import { render } from "./App.vue?vue&type=template&id=06e1648e"
+import script from "./App.vue?vue&type=script&lang=js"
+export * from "./App.vue?vue&type=script&lang=js"
+
+import exportComponent from "/Users/wallace/Desktop/CryptoTestament/code/app/cryptotestament/node_modules/vue-loader-v16/dist/exportHelper.js"
+const __exports__ = /*#__PURE__*/exportComponent(script, [['render',render]])
+
+export default __exports__
---
+++ webpack---/src/main.js
@@ -0,0 +1,13 @@
+import {
+    createApp
+} from 'vue';
+import App from './App.vue';
+import wallet from './wallet.js';
+
+
+async function load() {
+    await wallet.connect();
+    createApp(App).mount('#app');
+}
+
+load();
